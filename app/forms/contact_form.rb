463e67785730a9168ffb1c52dd6a5a408b602731
Luigi Ray-Montanez <luigi@codeforamerica.org>
2020-05-29 18:55:30 -0400

    Implement phone number on contact screen

--- app/forms/contact_form.rb
+++ app/forms/contact_form.rb
@@ -1,11 +1,15 @@
 class ContactForm < Form
-  set_attributes_for :household, :email_address
+  set_attributes_for :household, :email_address, :phone_number
   validates :email_address, format: { with: URI::MailTo::EMAIL_REGEXP, message: proc { I18n.t('validations.email_address') },
                                       if: :email_address_present? }
+  before_validation do
+    self.phone_number = phone_number.gsub(/[^0-9]/, '')
+  end
+  validates_length_of :phone_number, is: 10, if: :phone_number_present?,
+                                     message: I18n.t('validations.phone_number')
 
   def save
     attributes = attributes_for(:household)
-    attributes.except!(:email_address_check)
     household.update(attributes)
   end
 
@@ -13,5 +17,9 @@ class ContactForm < Form
 
   def email_address_present?
     email_address.present?
+  end
+
+  def phone_number_present?
+    phone_number.present?
   end
 end
--- spec/forms/contact_form_spec.rb
+++ spec/forms/contact_form_spec.rb
@@ -4,13 +4,22 @@ describe ContactForm do
   describe '#save' do
     it 'updates the existing household' do
       household = Household.create(is_eligible: :yes)
-      form = described_class.new(household, { email_address: 'test@test.com' })
+      form = described_class.new(household, { email_address: 'test@test.com', phone_number: '555-123-1234' })
       form.valid?
       form.save
 
       household.reload
 
       expect(household.email_address).to eq('test@test.com')
+      expect(household.phone_number).to eq('5551231234')
+    end
+
+    it 'handles invalid phone number' do
+      household = Household.create(is_eligible: :yes)
+      form = described_class.new(household, { phone_number: '123-3323' })
+
+      expect(form).not_to be_valid
+      expect(form.errors.first[1]).to eq('Please enter a valid phone number.')
     end
   end
 end
