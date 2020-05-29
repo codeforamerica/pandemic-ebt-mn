c2926eee932665431e1680ad357cf095f848282e
Matthew Schultz <abstractapproach@gmail.com>
2020-05-29 11:59:59 -0500

    MN Zip Codes (#13)
    * Change valid zips to MN list
    
    * Make tests pass with MN zip/address, DRY up mailing_address_form_spec
    
    * Hardcode MN instead of CA

--- spec/forms/mailing_address_form_spec.rb
+++ spec/forms/mailing_address_form_spec.rb
@@ -3,55 +3,47 @@ require 'faker'
 
 describe MailingAddressForm do
   describe '#save' do
+    before do
+      @household = Household.create(is_eligible: :yes)
+      @form = described_class.new(@household, { mailing_street: '1006 Summit Ave', mailing_street_2: 'Unit 3', mailing_city: 'St Paul',
+                                                mailing_zip_code: '55105' })
+    end
+
     it 'updates the existing household' do
-      household = Household.create(is_eligible: :yes)
-      form = described_class.new(household, { mailing_street: '123 Elm Street', mailing_street_2: 'Apt 2', mailing_city: 'Oakland',
-                                              mailing_zip_code: '96021' })
-      form.valid?
-      form.save
+      @form.valid?
+      @form.save
 
-      household.reload
+      @household.reload
 
-      expect(household.mailing_street).to eq('123 Elm Street')
-      expect(household.mailing_street_2).to eq('Apt 2')
-      expect(household.mailing_city).to eq('Oakland')
-      expect(household.mailing_zip_code).to eq('96021')
+      expect(@household.mailing_street).to eq('1006 Summit Ave')
+      expect(@household.mailing_street_2).to eq('Unit 3')
+      expect(@household.mailing_city).to eq('St Paul')
+      expect(@household.mailing_zip_code).to eq('55105')
     end
 
     it 'is valid without address 2 line' do
-      household = Household.create(is_eligible: :yes)
-      form = described_class.new(household, { mailing_street: '123 Elm Street', mailing_city: 'Oakland',
-                                              mailing_zip_code: '96021' })
-      expect(form).to be_valid
+      @form.mailing_street_2 = nil
+      expect(@form).to be_valid
     end
 
     it 'is valid with address 2 line' do
-      household = Household.create(is_eligible: :yes)
-      form = described_class.new(household, { mailing_street: '123 Elm Street', mailing_street_2: 'Apt 2', mailing_city: 'Oakland',
-                                              mailing_zip_code: '96021' })
-      expect(form).to be_valid
+      expect(@form).to be_valid
     end
 
     it 'is not valid if address 2 is too long' do
-      household = Household.create(is_eligible: :yes)
-      form = described_class.new(household, { mailing_street: '123 Elm Street', mailing_city: 'Oakland',
-                                              mailing_zip_code: '96021', mailing_street_2: Faker::String.random(length: 129) })
-      expect(form).not_to be_valid
-      expect(form.errors.first[1]).to eq('Please enter a shorter unit or apartment.')
+      @form.mailing_street_2 = Faker::String.random(length: 129)
+      expect(@form).not_to be_valid
+      expect(@form.errors.first[1]).to eq('Please enter a shorter unit or apartment.')
     end
 
     it 'validates zip codes' do
-      household = Household.create(is_eligible: :yes)
-      form = described_class.new(household, { mailing_street: '123 Elm Street', mailing_city: 'Oakland',
-                                              mailing_zip_code: 'not a zip code' })
-
-      expect(form).not_to be_valid
+      @form.mailing_zip_code = 'not a zip code'
+      expect(@form).not_to be_valid
     end
 
-    it 'validates that zip codes are from CA' do
-      household = Household.create(is_eligible: :yes)
-      form = described_class.new(household, { mailing_street: '1600 Pennsylvania Avenue NW', mailing_city: 'Washington D.C.',
-                                              mailing_zip_code: '20500' })
+    it 'validates that zip codes are from MN' do
+      form = described_class.new(@household, { mailing_street: '1600 Pennsylvania Avenue NW', mailing_city: 'Washington D.C.',
+                                               mailing_zip_code: '20500' })
 
       expect(form).not_to be_valid
     end
