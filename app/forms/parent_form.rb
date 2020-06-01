7bde64ca80469a7dd4cee3d4f84a36b060a911e7
Matthew Schultz <abstractapproach@gmail.com>
2020-06-01 09:16:56 -0500

    Add Parent View

--- app/forms/parent_form.rb
+++ app/forms/parent_form.rb
@@ -1,23 +1,23 @@
 class MailingAddressForm < Form
-  set_attributes_for :household, :parent_first_name, :parent_last_name, :dob_day, :dob_month, :dob_year
+  set_attributes_for :household, :parent_first_name, :parent_last_name, :parent_dob_day, :parent_dob_month, :parent_dob_year
   validates :parent_first_name, presence: { message: I18n.t('validations.first_name') }
   validates :parent_first_name, presence: { message: I18n.t('validations.last_name') }
-  validate :presence_of_dob_fields
+  validate :presence_of_parent_dob_fields
 
   def save
     form_attributes = attributes_for(:household)
     attributes = {
       first_name: form_attributes[:parent_first_name],
       last_name: form_attributes[:parent_last_name],
-      parent_dob: [form_attributes[:dob_day], form_attributes[:dob_month], form_attributes[:dob_year]].join('/')
+      parent_dob: [form_attributes[:parent_dob_day], form_attributes[:parent_dob_month], form_attributes[:parent_dob_year]].join('/')
     }
     household.update(attributes)
   end
 
   protected
 
-  def presence_of_dob_fields
-    %i[dob_year dob_month dob_day].detect do |attr|
+  def presence_of_parent_dob_fields
+    %i[parent_dob_year parent_dob_month parent_dob_day].detect do |attr|
       errors.add(:dob, proc { I18n.t('validations.dob') }) if public_send(attr).blank?
     end
   end
