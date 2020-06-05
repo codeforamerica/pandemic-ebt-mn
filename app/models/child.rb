e816f631f7076d743907340c75b746f3c1d27fe0
Cory Foy <github@cornetdesign.com>
2020-06-05 09:36:43 -0400

    Modifies the scopes for child to use include rather than joins

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,13 +1,13 @@
 class Child < ApplicationRecord
   belongs_to :household
 
-  scope :submitted, -> { joins(:household).where.not(households: { submitted_at: nil }) }
-  scope :unsubmitted, -> { joins(:household).where(households: { submitted_at: nil }) }
+  scope :submitted, -> { includes(:household).where.not(households: { submitted_at: nil }) }
+  scope :unsubmitted, -> { includes(:household).where(households: { submitted_at: nil }) }
 
   scope :submitted_after, ->(submitted_after_time) { submitted.where('households.submitted_at >= ?', submitted_after_time) }
   scope :submitted_before, ->(submitted_before_time) { submitted.where('households.submitted_at < ?', submitted_before_time) }
 
-  scope :by_household, ->(hhid) { joins(:household).where(households: { id: hhid }) }
+  scope :by_household, ->(hhid) { includes(:household).where(households: { id: hhid }) }
 
   def full_name
     "#{first_name} #{last_name}"
