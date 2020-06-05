751488469f2233aff0273587c9ffa665cba5e7b0
Cory Foy <github@cornetdesign.com>
2020-06-05 16:34:01 -0400

    Modifies the exporter to write all fields at once (#35)

--- app/models/child.rb
+++ app/models/child.rb
@@ -12,4 +12,60 @@ class Child < ApplicationRecord
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def csv_row
+    [
+      id,
+      first_name,
+      last_name,
+      dob,
+      school_registration_gender,
+      school_attended_name,
+      school_attended_grade,
+      household.signature,
+      household.mailing_street,
+      household.mailing_street_2,
+      household.mailing_city,
+      'MN',
+      household.mailing_zip_code,
+      household.parent_first_name,
+      household.parent_last_name,
+      household.parent_dob,
+      household.email_address,
+      household.phone_number,
+      household.language,
+      household.submitted_at,
+      household.application_experience,
+      household.experiment_group,
+      household.confirmation_code.to_s.delete('-')
+    ]
+  end
+
+  def self.csv_headers
+    %w[
+      child_id
+      student_first_name
+      student_last_name
+      student_dob
+      student_gender
+      student_school_name
+      student_school_grade
+      parent_signature
+      mailing_street
+      mailing_street_2
+      mailing_city
+      mailing_state
+      mailing_zip_code
+      parent_first_name
+      parent_last_name
+      parent_dob
+      email_address
+      phone_number
+      language
+      submitted_at
+      application_experience
+      experiment_group
+      maxis_id
+    ]
+  end
 end
