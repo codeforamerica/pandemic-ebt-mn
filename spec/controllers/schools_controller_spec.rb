3bb08c42e62c7759571facc31fe8ea09b4c4b606
Cory Foy <github@cornetdesign.com>
2020-06-05 19:03:09 -0400

    Modifies School Autocomplete to prioritize start matches

--- spec/controllers/schools_controller_spec.rb
+++ spec/controllers/schools_controller_spec.rb
@@ -9,25 +9,41 @@ RSpec.describe SchoolsController do
 
     it 'filters data when presented with a search term' do
       get :index, params: { term: 'High' }
-      expected = [{ value: '270 Hopkins North Junior High ALC', label: '270 Hopkins North Junior High ALC' }, { value: '270 Hopkins North Junior High TS', label: '270 Hopkins North Junior High TS' }, { value: '270 Hopkins West Junior High ALC', label: '270 Hopkins West Junior High ALC' }].to_json
+      expected = [
+        'High School For Recording Arts',
+        'Highwood Hills Elementary',
+        'Highview Middle School'
+      ].map { |e| { value: e, label: e } }.to_json
       expect(response.body).to eq(expected)
     end
 
     it 'filters data when presented with a partial search term' do
       get :index, params: { term: 'Mid' }
-      expected = [{ value: '112 ALC Middle School', label: '112 ALC Middle School' }, { value: '272 Central Mid Sch TS', label: '272 Central Mid Sch TS' }, { value: '273 So View Middle TS', label: '273 So View Middle TS' }].to_json
+      expected = [
+        'Midway Star Academy',
+        'Middle Area Learning Center',
+        'Middle Level Alternative Program'
+      ].map { |e| { value: e, label: e } }.to_json
       expect(response.body).to eq(expected)
     end
 
     it 'filters data regardless of case' do
       get :index, params: { term: 'mid' }
-      expected = [{ value: '112 ALC Middle School', label: '112 ALC Middle School' }, { value: '272 Central Mid Sch TS', label: '272 Central Mid Sch TS' }, { value: '273 So View Middle TS', label: '273 So View Middle TS' }].to_json
+      expected = [
+        'Midway Star Academy',
+        'Middle Area Learning Center',
+        'Middle Level Alternative Program'
+      ].map { |e| { value: e, label: e } }.to_json
       expect(response.body).to eq(expected)
     end
 
     it 'returns all data when search term is empty' do
       get :index, params: { term: '' }
-      expected = [{ value: '112 ALC Independent Study', label: '112 ALC Independent Study' }, { value: '112 ALC Middle School', label: '112 ALC Middle School' }, { value: '112 ALC Seat-Based', label: '112 ALC Seat-Based' }].to_json
+      expected = [
+        '112 ALC Independent Study',
+        '112 ALC Middle School',
+        '112 ALC Seat-Based'
+      ].map { |e| { value: e, label: e } }.to_json
       expect(response.body).to eq(expected)
     end
 
@@ -39,7 +55,11 @@ RSpec.describe SchoolsController do
 
     it 'limits the results to 3 items, sorted alphabetically' do
       get :index, params: { term: 'Elementary' }
-      expected =  [{ value: '270 - Glen Lake Elementary', label: '270 - Glen Lake Elementary' }, { value: '270 Alice Smith Elementary TS', label: '270 Alice Smith Elementary TS' }, { value: '270 Eisenhower Elementary TS', label: '270 Eisenhower Elementary TS' }].to_json
+      expected = [
+        'Elementary Lighthouse',
+        'Elementary ALC Targeted Services',
+        'Elementary Targeted Services'
+      ].map { |e| { value: e, label: e } }.to_json
       expect(response.body).to eq(expected)
     end
   end
--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -2,25 +2,27 @@ require 'rails_helper'
 
 describe 'School' do
   before do
-    stub_const("SCHOOL_LIST", [
-      "A Great School",
-      "Another Great School",
-      "Best School Ever",
-      "Completely Awesome School",
-      "Great Expectations",
-      "Superschool of Awesome",
-      "Superschool of OK",
-      "Superschool of Superheroes",
-      "The Greatest School"
-    ])
+    stub_const('SCHOOL_LIST', [
+                 'A Great School',
+                 'Another Great School',
+                 'Best School Ever',
+                 'Completely Awesome School',
+                 'Great Expectations',
+                 'Superschool of Awesome',
+                 'Superschool of OK',
+                 'Superschool of Superheroes',
+                 'The Greatest School',
+                 'The Superest School'
+               ])
   end
 
   describe '#where' do
     it 'finds results that start with the term' do
       expected = [
-        "Superschool of Awesome",
-        "Superschool of OK",
-        "Superschool of Superheroes"
+        'Superschool of Awesome',
+        'Superschool of OK',
+        'Superschool of Superheroes',
+        'The Superest School'
       ]
       actual = School.where('Super')
       expect(actual).to eq(expected)
@@ -28,8 +30,8 @@ describe 'School' do
 
     it 'finds results that include the term' do
       expected = [
-        "Completely Awesome School",
-        "Superschool of Awesome"
+        'Completely Awesome School',
+        'Superschool of Awesome'
       ]
       actual = School.where('Awesome')
       expect(actual).to eq(expected)
@@ -37,8 +39,8 @@ describe 'School' do
 
     it 'finds results regardless of case' do
       expected = [
-        "Completely Awesome School",
-        "Superschool of Awesome"
+        'Completely Awesome School',
+        'Superschool of Awesome'
       ]
       actual = School.where('awesome')
       expect(actual).to eq(expected)
@@ -46,7 +48,7 @@ describe 'School' do
 
     it 'finds results that end with the term' do
       expected = [
-        "Best School Ever"
+        'Best School Ever'
       ]
       actual = School.where('Ever')
       expect(actual).to eq(expected)
@@ -56,23 +58,34 @@ describe 'School' do
   describe '#find_sorted_by_term' do
     it 'limits results to 3' do
       expected = [
-        "Superschool of Awesome",
-        "Superschool of OK",
-        "Superschool of Superheroes"
-      ].map { |e| {label: e, value: e} }
+        'Superschool of Awesome',
+        'Superschool of OK',
+        'Superschool of Superheroes'
+      ].map { |e| { label: e, value: e } }
       actual = School.find_sorted_by_term('Super')
       expect(actual).to eq(expected)
     end
 
     it 'prioritizes results that start with the term even when not alphabetically first' do
       expected = [
-        "Superschool of Awesome",
-        "Superschool of OK",
-        "Superschool of Superheroes"
-      ].map { |e| {label: e, value: e} }
-      actual = School.find_sorted_by_term('Super')
+        'Great Expectations',
+        'A Great School',
+        'Another Great School'
+      ].map { |e| { label: e, value: e } }
+      actual = School.find_sorted_by_term('Great')
+      expect(actual).to eq(expected)
+    end
+
+    it 'handles cases where no data is returned' do
+      expected = []
+      actual = School.find_sorted_by_term('ZZZZZZZZ')
+      expect(actual).to eq(expected)
+    end
+
+    it 'handles cases where term is nil by returning the first 3 records' do
+      expected = 3
+      actual = School.find_sorted_by_term(nil).length
       expect(actual).to eq(expected)
     end
   end
 end
-
