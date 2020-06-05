eaab76c0a8287020724a628db9e6dcfb4dfb562e
Matthew Schultz <abstractapproach@gmail.com>
2020-06-05 14:53:05 -0500

    Restore ES (#34)
    * Restore language spec; change to use root
    
    * Restore ES

--- spec/features/info_page_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'Info page', type: :feature do
-  skip 'switches language to Spanish' do
-    visit '/en/info'
-    click_on 'Español'
-    expect(page).to have_text 'Información sobre P-EBT'
-  end
-end
