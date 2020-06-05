e9ba8deee00c402619544268de73187129acb1bc
Cory Foy <github@cornetdesign.com>
2020-06-05 19:03:09 -0400

    Modifies School Autocomplete to prioritize start matches

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,7 +2,19 @@ class School
   MAX_RESULTS = 3
 
   def self.find_sorted_by_term(term)
+    term = '' if term.nil?
     results = where(term)
+    if term.present?
+      results = results.sort do |r1, r2|
+        if r1.downcase.starts_with?(term.downcase)
+          -1
+        elsif r2.downcase.starts_with?(term.downcase)
+          1
+        else
+          r1 <=> r2
+        end
+      end
+    end
     results.take(MAX_RESULTS).map do |name|
       {
         value: name,
