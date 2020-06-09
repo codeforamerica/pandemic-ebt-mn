7575fd7b476fbaf3f8cadce3b83d67ef5017466c
Cory Foy <github@cornetdesign.com>
2020-06-09 17:25:50 -0400

    Updates the signature page to handle null bytes being sent

--- app/forms/signature_form.rb
+++ app/forms/signature_form.rb
@@ -1,11 +1,17 @@
 class SignatureForm < Form
+  NULL_BYTE = "\u0000".freeze
   set_attributes_for :household, :signature
   validates_presence_of :signature, message: proc { I18n.t('validations.signature') }
 
   def save
     Household.transaction do
       Household.connection.execute('LOCK households IN ACCESS EXCLUSIVE MODE')
-      household.update(attributes_for(:household).merge({ submitted_at: Time.zone.now, huid: (household.huid || Household.next_huid) }))
+      form_attributes = attributes_for(:household)
+      attributes = {
+        signature: form_attributes[:signature].gsub(NULL_BYTE, ' ').strip,
+        huid: (household.huid || Household.next_huid)
+      }
+      household.update(attributes.merge({ submitted_at: Time.zone.now }))
     end
   end
 end
