8be917d4b547e4b489209186f6c78900aea798a8
Cory Foy <github@cornetdesign.com>
2020-06-12 13:42:45 -0400

    Changes language selection to be a dropdown (#54)
    In preparation for multiple languages we changed it to be a dropdown. Only implements Spanish and English right now

--- spec/features/translation_spec.rb
+++ spec/features/translation_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe 'translation', type: :feature do
+RSpec.describe 'translation', type: :feature, js: true do
   it 'switches language to Spanish' do
     visit '/en'
-    click_on 'Español'
+    select 'Español'
     expect(page).to have_text 'Reciba ayuda para comprar comida mientras escuelas están cerradas.'
   end
 end
