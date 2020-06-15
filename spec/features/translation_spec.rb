a5008c7a26286d24d78eb659862a58dd7d3a6594
Cory Foy <github@cornetdesign.com>
2020-06-15 12:42:10 -0400

    Pulls out the language drop down until we get more languages

--- spec/features/translation_spec.rb
+++ spec/features/translation_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'translation', type: :feature, js: true do
   it 'switches language to Spanish' do
     visit '/en'
-    select 'Español'
+    click_on 'Español'
     expect(page).to have_text 'Reciba ayuda para comprar comida mientras escuelas están cerradas.'
   end
 end
