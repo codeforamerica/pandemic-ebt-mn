4dc85e6c74ea739ab4fe701af03eb625988bc567
Matthew Schultz <abstractapproach@gmail.com>
2020-06-22 16:02:49 -0500

    new translations/languages

--- spec/features/translation_spec.rb
+++ spec/features/translation_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'translation', type: :feature, js: true do
   it 'switches language to Spanish' do
     visit '/en'
-    click_on 'Español'
+    select 'Español', from: 'language_select'
     expect(page).to have_text 'Reciba ayuda para comprar comida mientras escuelas están cerradas.'
   end
 end
