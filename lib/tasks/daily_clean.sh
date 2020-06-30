9f04cc39aee71bbbf52f11478f20d15704d6a778
Ben Calegari <bcalegari@codeforamerica.org>
2020-06-30 16:01:11 -0700

    Append state to cronitor api call
    - Only set -x so that we can catch the status

--- lib/tasks/daily_clean.sh
+++ lib/tasks/daily_clean.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-set -xe
+set -x
 
 rails_env=$1
-curl https://cronitor.link/KuHCa7/run?msg="Daily clean started on: ${rails_env}" -m 10 || true
+curl https://cronitor.link/KuHCa7/run?msg="Daily clean started on: MN-${rails_env}" -m 10 || true
 
 cd /app || exit
 result=$(bundle exec thor clean:addresses)
 status_code=$?
 addresses=$(echo "${result}" | tail -n1 | cut -c26-27)
 
-curl https://cronitor.link/KuHCa7/complete?msg="Daily clean completed on: ${rails_env}, Status: ${status_code}, Addresses Cleaned: ${addresses}" -m 10 || true
+curl https://cronitor.link/KuHCa7/complete?msg="Daily clean completed on: MN-${rails_env}, Status: ${status_code}, Addresses Cleaned: ${addresses}" -m 10 || true
--- lib/tasks/weekly_export.sh
+++ lib/tasks/weekly_export.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-set -xe
+set -x
 
 rails_env=$1
-curl https://cronitor.link/yITppB/run?msg="Weekly export started on: ${rails_env}" -m 10 || true
+curl https://cronitor.link/KuHCa7/run?msg="Weekly export started on: MN-${rails_env}" -m 10 || true
 
 cd /app || exit
 today=$(date +'%Y-%m-%d')
-result=$(bundle exec thor clean:addresses \
+bundle exec thor clean:addresses \
   && bundle exec thor export:last_x_days 7 tmp/"${today}".csv \
-  && bundle exec thor export:upload_export_to_aws tmp/"${today}".csv)
+  && bundle exec thor export:upload_export_to_aws tmp/"${today}".csv
 status_code=$?
 
-curl https://cronitor.link/yITppB/complete?msg="Weekly export completed on: ${rails_env}, Status: ${status_code}" -m 10 || true
+curl https://cronitor.link/KuHCa7/complete?msg="Weekly export completed on: MN-${rails_env}, Status: ${status_code}" -m 10 || true
