e0cda283516001525aa5adb178fc2bcfdd06430c
Ben Calegari <bcalegari@codeforamerica.org>
2020-07-06 14:55:33 -0700

    Make it possible to rollback from default state migration

--- db/migrate/20200702003624_add_default_state_to_households.rb
+++ db/migrate/20200702003624_add_default_state_to_households.rb
@@ -1,6 +1,11 @@
 class AddDefaultStateToHouseholds < ActiveRecord::Migration[6.0]
-  def change
+  def up
     Household.where(mailing_state: nil).update_all mailing_state: 'MN'
     change_column :households, :mailing_state, :string, limit: 2, null: false, default: 'MN'
+  end
+
+  def down
+    Household.where(mailing_state: nil).update_all mailing_state: 'MN'
+    change_column :households, :mailing_state, :string, limit: 2, null: true
   end
 end
