194323620f3aa203ee5fc3b021ba304fdb61ff12
Luigi Ray-Montanez <luigi@codeforamerica.org>
2020-07-09 11:59:46 -0400

    Avoid http_auth on this endpoint
    [#173612924]

--- app/controllers/autoresponse_controller.rb
+++ app/controllers/autoresponse_controller.rb
@@ -1,9 +1,4 @@
-class AutoresponseController < ApplicationController
-  skip_before_action :http_authenticate
-  skip_before_action :set_sentry_context
-  skip_before_action :check_locale
-  skip_around_action :switch_locale
-
+class AutoresponseController < ActionController::Base
   def autoresponse
     sender = params['sender']
     AutoresponseMailer.new.mail(sender)
