2696255d66dd4fd7d1e3c19049b4e8fc6342acef
Luigi Ray-Montanez <luigi@codeforamerica.org>
2020-07-09 12:36:56 -0400

    Avoid CSRF token authentication on this endpoint
    [#173612924]

--- app/controllers/autoresponse_controller.rb
+++ app/controllers/autoresponse_controller.rb
@@ -1,4 +1,6 @@
 class AutoresponseController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+
   def autoresponse
     sender = params['sender']
     AutoresponseMailer.new.mail(sender)
