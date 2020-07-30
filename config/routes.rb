3ed18e12e351da1d51e38b0ea66118d2df93d5ac
Luigi Ray-Montanez <luigi@codeforamerica.org>
2020-07-30 11:46:47 -0400

    Closing time 🚪
    [#173985703]

--- config/routes.rb
+++ config/routes.rb
@@ -9,26 +9,28 @@ Rails.application.routes.draw do
   get '/schools' => 'schools#index'
   get '/:locale' => 'pages#index'
 
-  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
-    get 'early' => 'pages#early', as: 'early'
-    get 'how' => 'pages#how', as: 'how'
-    get 'card' => 'pages#card', as: 'card'
-    get 'done' => 'pages#done', as: 'done'
+  if ENV['CLOSED'] != '1'
+    scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+      get 'early' => 'pages#early', as: 'early'
+      get 'how' => 'pages#how', as: 'how'
+      get 'card' => 'pages#card', as: 'card'
+      get 'done' => 'pages#done', as: 'done'
 
-    resources :steps, controller: :forms, only: (Rails.env.production? ? %i[show] : %i[show index]) do
-      collection do
-        FormNavigation.controllers.uniq.each do |controller_class|
-          { get: :edit, put: :update }.each do |method, action|
-            match "/#{controller_class.to_param}",
-                  action: action,
-                  controller: controller_class.controller_path,
-                  via: method
+      resources :steps, controller: :forms, only: (Rails.env.production? ? %i[show] : %i[show index]) do
+        collection do
+          FormNavigation.controllers.uniq.each do |controller_class|
+            { get: :edit, put: :update }.each do |method, action|
+              match "/#{controller_class.to_param}",
+                    action: action,
+                    controller: controller_class.controller_path,
+                    via: method
+            end
           end
         end
       end
-    end
 
-    delete '/steps/children/:id' => 'children#destroy', as: 'remove_child'
+      delete '/steps/children/:id' => 'children#destroy', as: 'remove_child'
+    end
   end
   mount Cfa::Styleguide::Engine => '/cfa'
   match '*path', to: redirect { |_path, req| req.flash[:error] = 'There has been a problem on our end.'; '/en' }, via: :all
